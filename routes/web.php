f7fcb1430eef8ef05bcf8e266cfd9053a7d2d27d
iatnav222 <tranvntai222@gmail.com>
2026-03-12 23:48:57 +0700

    cap nhat lai route sua lai phuong phap redirect

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,22 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes - Quản trị (Admin CRUD)
-|--------------------------------------------------------------------------
-*/
+// 1. Ý TƯỞNG MỚI: Vào link gốc là vào ngay trang quản trị
+Route::get('/', function () {
+    return redirect('/users-admin');
+});
 
+// 2. Giao diện quản trị
 Route::get('/users-admin', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::put('/users/{id}', [UserController::class, 'update']);
 Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
-/*
-|--------------------------------------------------------------------------
-| API Routes - Theo đúng yêu cầu của thầy (JSON)
-|--------------------------------------------------------------------------
-*/
-
-// BASE_API/users -> Danh sách tất cả users
+// 3. API cho Frontend & Thầy test (JSON)
 Route::get('/users', function () {
     return response()->json(User::all(), 200, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
-// BASE_API/users/{id} -> User cụ thể
 Route::get('/users/{id}', function ($id) {
     $user = User::find($id);
     if($user) {
@@ -37,12 +30,7 @@ Route::get('/users/{id}', function ($id) {
     return response()->json(['message' => 'User not found'], 404, [], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 });
 
-/*
-|--------------------------------------------------------------------------
-| Utility Routes - Cứu hộ
-|--------------------------------------------------------------------------
-*/
-
+// 4. Route cứu hộ
 Route::get('/init-db', function () {
     try {
         Artisan::call('migrate', ['--force' => true]);
