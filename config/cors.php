e57228085229459add84e9edded8eebd4c18b146
iatnav222 <tranvntai222@gmail.com>
2026-03-13 13:07:15 +0700

    sua file cors

--- config/cors.php
+++ config/cors.php
@@ -6,16 +6,10 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    // ĐÃ SỬA DÒNG NÀY: Thêm 'users' và 'users/*' vào danh sách cho phép
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'users', 'users/*'],
 
     'allowed_methods' => ['*'],
 
